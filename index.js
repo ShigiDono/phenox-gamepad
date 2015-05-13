e5b9bf6a023084049aebcac7f239a7f50496ac70
root <root@linaro-ubuntu-desktop.(none)>
2015-05-13 05:58:26 +0000

    Working version

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var util = require('util');
 var net = require('net');
 var express = require('express');
 var app = express();
@@ -8,19 +9,28 @@ var phenox = require('./node_modules/phenox/build/Release/phenox');
 var fs = require('fs');
 var socket;
 phenox.init();
+function toBuffer(ab) {
+    var buffer = new Buffer(ab.length);
+    var view = ab;//nbew Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        buffer[i] = view.array[i];
+    }
+    return buffer;
+}
 setInterval(function() {
-    var send_data = phenox.get_data();
+    var send_data = toBuffer(phenox.get_data());
     ws.clients.forEach(function(client) { 
-        console.log(send_data.byteLength);
-        //client && client.send(send_data);     
-        //console.log(client.bufferedAmount);
         if(client.bufferedAmount == 0) {
             client.send(send_data,{binary: true});
         }
     });
-}, 100);
+}, 33);
 
-
+ws.on('request', function(request) {
+    var connection = request.accept(null, request.origin); 
+    connection.binaryType = "arraybuffer";
+    console.log(1);
+});
 ws.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log(message);
