115cf34cbbbb90c2e7284e18d0ea5d7d7e86225a
Peter Khrapchenkov <shigidono@gmail.com>
2015-05-13 19:08:33 +1000

    Fix angles

--- index.js
+++ index.js
@@ -45,9 +45,12 @@ ws.on('connection', function(ws) {
                 phenox.go_down();
             }
         } else {
-            phenox.set_angles(obj.a.dx, obj.a.dy, obj.b.dx);//obj.b.dx
-            if (obj.b.dy != 0) {
+            phenox.set_angles(obj.a.dx/15, obj.a.dy/15, obj.b.dx/15);//obj.b.dx
+            if (obj.b.dy > 100) {
+                phenox.go_up();
 
+            } else if (obj.b.dy < -100) {
+                phenox.go_down();
             }
         }
     });
