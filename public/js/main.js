5b0b3c653eafd0c3ae9bb36c27405f68a7a9a4e4
Peter Khrapchenkov <shigidono@gmail.com>
2015-05-22 20:39:00 +1000

    Small change(again)

--- public/js/main.js
+++ public/js/main.js
@@ -345,7 +345,7 @@ void main(void) {\n\
                 //gl.bindTexture(gl.TEXTURE_2D, null);
                 gl.bindFramebuffer(gl.FRAMEBUFFER, null);
                 gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
-                t.draw(s, rttTexture);
+                t.draw(s, {texture:rttTexture});
             }
             requestAnimationFrame(draw);
         }
