476d2724f8a054e7263a249218bf2d0e19badc8d
Peter Khrapchenkov <shigidono@gmail.com>
2015-05-22 20:27:39 +1000

    Small changes

--- public/js/main.js
+++ public/js/main.js
@@ -271,11 +271,18 @@ void main(void) {\n\
 
             rttTexture = gl.createTexture();
             gl.bindTexture(gl.TEXTURE_2D, rttTexture);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+            gl.texParameteri( 
+                gl.TEXTURE_2D,
+                gl.TEXTURE_MAG_FILTER,
+                gl.LINEAR );
 
+            gl.texParameteri( 
+                gl.TEXTURE_2D, 
+                gl.TEXTURE_MIN_FILTER, 
+                gl.LINEAR );
+
+            gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, rttFramebuffer.width, rttFramebuffer.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 
 
@@ -338,6 +345,7 @@ void main(void) {\n\
                 //gl.bindTexture(gl.TEXTURE_2D, null);
                 gl.bindFramebuffer(gl.FRAMEBUFFER, null);
                 gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+                t.draw(s, rttTexture);
             }
             requestAnimationFrame(draw);
         }
